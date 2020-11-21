ba74139d9955ea599baa06c474c2d9d0a1ea4c55
MrAndreaz <andreaszaritskiy@yandex.ru>
2020-11-21 17:42:22 +0300

    висилица

---
+++ вислица 2.py
@@ -0,0 +1,16 @@
+from random import randint
+word = ("австралия")
+dedant = 10
+game = True
+while game:
+    letter = input ("Введите букву или слово: ")
+    if letter in word:
+        print ("Есть такая буква!")
+    else:
+        print ("Не подходит.")
+        dedant = dedant - 1
+        print ("Жизни: " + str(dedant))
+
+    if letter == word:
+        print ("ТЫ ПОБЕДИЛ! Игра окончена")
+        game = False
